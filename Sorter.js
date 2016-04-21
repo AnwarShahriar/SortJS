58ef233a774aa8e3ceaef888dfa25bc9187ea254
shishir <anwarshahriar@gmail.com>
2016-04-21 12:51:17 +0600

    Adds merge sort algorithm

--- Sorter.js
+++ Sorter.js
@@ -23,6 +23,7 @@ module.exports = class Sorter {
     
     mergeSort(tobeSorted) {
         let arr = tobeSorted.slice();
+        mergeSortAlg(arr, 0, arr.length - 1);
         return arr;
     }
     
@@ -31,3 +32,49 @@ module.exports = class Sorter {
         return arr;
     }
 }
+var mergeSortAlg = function (arr, p, r) {
+    if (p < r) {
+        let q = Math.floor((p + r) / 2);
+        mergeSortAlg(arr, p, q);
+        mergeSortAlg(arr, q + 1, r);
+        merge(arr, p, q, r);
+    }    
+};
+
+var merge = function (arr, p, q, r) {
+    let lowHalf = [];
+    let highHalf = [];
+    
+    for(let i = 0, k = p; k <= q; i++, k++) {
+        lowHalf[i] = arr[k];
+    }
+    
+    for(let i = 0, k = q + 1; k <= r; i++, k++) {
+        highHalf[i] = arr[k];
+    }
+    
+    let i = 0, j = 0, k = p;
+    while (i < lowHalf.length && j < highHalf.length) {
+        if (lowHalf[i] < highHalf[j]) {
+            arr[k] = lowHalf[i];
+            k++;
+            i++;
+        } else {
+            arr[k] = highHalf[j];
+            k++;
+            j++;
+        }
+    }
+    
+    while (i < lowHalf.length) {
+        arr[k] = lowHalf[i];
+        k++;
+        i++;
+    }
+    
+    while (j < highHalf.length) {
+        arr[k] = highHalf[j];
+        k++;
+        j++;
+    }
+};
--- test.js
+++ test.js
@@ -3,7 +3,6 @@ let Sorter = require('./Sorter.js');
 let sorter = new Sorter();
 
 const dataSet = [9, 8, 7, 6, 9, 4, 1, 0, 3, 0, 1];
-
 console.log("BubbleSort: ", sorter.bubbleSort( dataSet ) );
-console.log("MergeSort: ", sorter.mergeSort( dataSet ) );
+console.log("MergeSort: ", sorter.mergeSort ( dataSet ) );
 console.log("QuickSort: ", sorter.quickSort( dataSet ) );
